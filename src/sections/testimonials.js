eedd095abec8d9123598a9ec3026b6e7e4b71caf
allenwu9999 <b08901057@ntu.edu.tw>
2021-02-04 23:24:41 +0800

    bug needed to fix

--- src/sections/testimonials.js
+++ src/sections/testimonials.js
@@ -89,7 +89,10 @@ const Testimonials = ({ contentModuleId }) => {
       id="testimonials"
       className="testimonials container section mx-auto"
     >
-      <div className="w-full md:w-1/2 pl-0 md:pl-16 text-center md:text-left">
+      <div
+        className="w-full md:w-1/2 pl-0 md:pl-16 text-center md:text-left"
+        style={{ marginRight: "10px" }}
+      >
         <h1
           className="w-full md:w-3/4 font-bold text-5xl leading-none mb-6"
           data-sal="fade"
@@ -98,31 +101,69 @@ const Testimonials = ({ contentModuleId }) => {
           大會獎
         </h1>
         <p>maybe put some images here...</p>
-      </div>
-      <div className="w-full md:w-1/2 pt-12 md:pt-0">
-        {content.length > 0 && (
-          <div
-            className="testimonial__slider"
-            data-sal="fade"
-            data-sal-easing="ease-in-cubic"
-          >
-            <div className="glider">
-              {content.map((testimonial) => (
-                <Testimonial
-                  testimonial={testimonial.node}
-                  key={testimonial.node.id}
-                />
-              ))}
+        <div className="w-full pt-12 md:pt-0">
+          {content.length > 0 && (
+            <div
+              className="testimonial__slider"
+              data-sal="fade"
+              data-sal-easing="ease-in-cubic"
+            >
+              <div className="glider">
+                {content.map((testimonial) => (
+                  <Testimonial
+                    testimonial={testimonial.node}
+                    key={testimonial.node.id}
+                  />
+                ))}
+              </div>
+              <button className="glider-prev material-icons">
+                keyboard_arrow_left
+              </button>
+              <button className="glider-next material-icons">
+                keyboard_arrow_right
+              </button>
+              <div className="glider__dots"></div>
             </div>
-            <button className="glider-prev material-icons">
-              keyboard_arrow_left
-            </button>
-            <button className="glider-next material-icons">
-              keyboard_arrow_right
-            </button>
-            <div className="glider__dots"></div>
-          </div>
-        )}
+          )}
+        </div>
+      </div>
+      <div
+        className="w-full md:w-1/2 pl-0 md:pl-16 text-center md:text-left"
+        style={{ marginLeft: "10px" }}
+      >
+        <h1
+          className="w-full md:w-3/4 font-bold text-5xl leading-none mb-6"
+          data-sal="fade"
+          data-sal-easing="ease-in-cubic"
+        >
+          企業獎
+        </h1>
+        <p>maybe put some images here...</p>
+        <div className="w-full pt-12 md:pt-0">
+          {content.length > 0 && (
+            <div
+              className="testimonial__slider"
+              data-sal="fade"
+              data-sal-easing="ease-in-cubic"
+            >
+              <div className="glider">
+                {content.map((testimonial) => (
+                  <Testimonial
+                    testimonial={testimonial.node}
+                    key={testimonial.node.id}
+                  />
+                ))}
+              </div>
+              <button className="glider-prev material-icons">
+                keyboard_arrow_left
+              </button>
+              <button className="glider-next material-icons">
+                keyboard_arrow_right
+              </button>
+              <div className="glider__dots"></div>
+            </div>
+          )}
+        </div>
       </div>
     </section>
   );
