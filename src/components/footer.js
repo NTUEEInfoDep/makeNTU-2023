5c9dd01e341a6e26b0fb4cb839c180f25678bbcb
Fiona <fionawu@genius-bull.com>
2021-02-15 13:52:09 +0800

    fix style

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,138 +3,138 @@ import PropTypes from "prop-types";
 import { useStaticQuery, graphql } from "gatsby";
 
 const Footer = ({ menus }) => {
-  const data = useStaticQuery(graphql`
-    query {
-      contentfulContactDetails {
-        contactInfo
-        id
-        addressLine1
-        addressLine2
-        contactNumber
-        email
-        menuLinks
-        followUs
-        facebookUrl
-        twitterUrl
-        instagramUrl
-      }
-    }
-  `);
+    const data = useStaticQuery(graphql`
+        query {
+            contentfulContactDetails {
+                contactInfo
+                id
+                addressLine1
+                addressLine2
+                contactNumber
+                email
+                menuLinks
+                followUs
+                facebookUrl
+                twitterUrl
+                instagramUrl
+            }
+        }
+    `);
 
-  const footerMenu =
-    menus !== null && menus !== undefined
-      ? menus.find((menu) => menu.type === "secondary")
-      : null;
+    const footerMenu = menus !== null && menus !== undefined ? menus.find((menu) => menu.type === "secondary") : null;
 
-  return (
-    <footer className="footer bg-tertiary text-white">
-      <div className="container section mx-auto py-10">
-        <div className="footer__content">
-          <h3 className="text-lg font-bold mb-4 text-lightGray">
-            {data.contentfulContactDetails.contactInfo}
-          </h3>
-          <ul className="text-sm">
-            <li className="mb-2">
-              <div className="item">
-                <i className="item__icon material-icons text-lightGray text-2xl">
-                  business
-                </i>
-                <div className="item__content">
-                  <p className="item__text">
-                    {data.contentfulContactDetails.addressLine1}
-                    <br />
-                    {data.contentfulContactDetails.addressLine2}
-                  </p>
+    return (
+        <footer className="footer bg-tertiary text-white">
+            <div className="container section mx-auto py-10">
+                <div className="footer__content">
+                    <h3 className="text-lg font-bold mb-4 text-lightGray">
+                        {data.contentfulContactDetails.contactInfo}
+                    </h3>
+                    <ul className="text-sm">
+                        <li className="mb-2">
+                            <div className="item">
+                                <i
+                                    className="item__icon material-icons text-lightGray text-2xl"
+                                    style={{ color: "white" }}
+                                >
+                                    business
+                                </i>
+                                <div className="item__content">
+                                    <p className="item__text">
+                                        {data.contentfulContactDetails.addressLine1}
+                                        <br />
+                                        {data.contentfulContactDetails.addressLine2}
+                                    </p>
+                                </div>
+                            </div>
+                        </li>
+                        <li className="mb-2">
+                            <div className="item">
+                                <i
+                                    className="item__icon material-icons text-lightGray text-2xl"
+                                    style={{ color: "white" }}
+                                >
+                                    settings_phone
+                                </i>
+                                <div className="item__content">
+                                    <p className="item__text">{data.contentfulContactDetails.contactNumber}</p>
+                                </div>
+                            </div>
+                        </li>
+                        <li className="mb-2">
+                            <div className="item">
+                                <i
+                                    className="item__icon material-icons text-lightGray text-2xl"
+                                    style={{ color: "white" }}
+                                >
+                                    email
+                                </i>
+                                <div className="item__content">
+                                    <p className="item__text">{data.contentfulContactDetails.email}</p>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
                 </div>
-              </div>
-            </li>
-            <li className="mb-2">
-              <div className="item">
-                <i className="item__icon material-icons text-lightGray text-2xl">
-                  settings_phone
-                </i>
-                <div className="item__content">
-                  <p className="item__text">
-                    {data.contentfulContactDetails.contactNumber}
-                  </p>
+                {footerMenu !== null && footerMenu !== undefined && (
+                    <div className="footer__content">
+                        <h3 className="text-lg font-bold mb-4 text-lightGray">
+                            {data.contentfulContactDetails.menuLinks}
+                        </h3>
+                        <ul className="text-sm">
+                            {footerMenu.menuItems.map((menu) => (
+                                <li className="mb-2" key={menu.id}>
+                                    <a className="hover:text-primary" href={menu.url}>
+                                        {menu.title}
+                                    </a>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                )}
+                <div className="footer__content">
+                    <h3 className="text-lg font-bold mb-4 text-lightGray">{data.contentfulContactDetails.followUs}</h3>
+                    <ul className="flex text-sm">
+                        <li className="mb-2 mr-4">
+                            <a href={data.contentfulContactDetails.facebookUrl}>
+                                <img
+                                    className="w-6 h-6"
+                                    src={require("assets/images/facebook.svg")}
+                                    alt="Facebook Icon"
+                                />
+                            </a>
+                        </li>
+                        <li className="mb-2 mr-4">
+                            <a href={data.contentfulContactDetails.twitterUrl}>
+                                <img
+                                    className="w-6 h-6"
+                                    src={require("assets/images/twitter.svg")}
+                                    alt="Twitter Icon"
+                                />
+                            </a>
+                        </li>
+                        <li className="mb-2 mr-4">
+                            <a href={data.contentfulContactDetails.instagramUrl}>
+                                <img
+                                    className="w-6 h-6"
+                                    src={require("assets/images/instagram.svg")}
+                                    alt="Instagram Icon"
+                                />
+                            </a>
+                        </li>
+                    </ul>
                 </div>
-              </div>
-            </li>
-            <li className="mb-2">
-              <div className="item">
-                <i className="item__icon material-icons text-lightGray text-2xl">
-                  email
-                </i>
-                <div className="item__content">
-                  <p className="item__text">
-                    {data.contentfulContactDetails.email}
-                  </p>
-                </div>
-              </div>
-            </li>
-          </ul>
-        </div>
-        {footerMenu !== null && footerMenu !== undefined && (
-          <div className="footer__content">
-            <h3 className="text-lg font-bold mb-4 text-lightGray">
-              {data.contentfulContactDetails.menuLinks}
-            </h3>
-            <ul className="text-sm">
-              {footerMenu.menuItems.map((menu) => (
-                <li className="mb-2" key={menu.id}>
-                  <a className="hover:text-primary" href={menu.url}>
-                    {menu.title}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </div>
-        )}
-        <div className="footer__content">
-          <h3 className="text-lg font-bold mb-4 text-lightGray">
-            {data.contentfulContactDetails.followUs}
-          </h3>
-          <ul className="flex text-sm">
-            <li className="mb-2 mr-4">
-              <a href={data.contentfulContactDetails.facebookUrl}>
-                <img
-                  className="w-6 h-6"
-                  src={require("assets/images/facebook.svg")}
-                  alt="Facebook Icon"
-                />
-              </a>
-            </li>
-            <li className="mb-2 mr-4">
-              <a href={data.contentfulContactDetails.twitterUrl}>
-                <img
-                  className="w-6 h-6"
-                  src={require("assets/images/twitter.svg")}
-                  alt="Twitter Icon"
-                />
-              </a>
-            </li>
-            <li className="mb-2 mr-4">
-              <a href={data.contentfulContactDetails.instagramUrl}>
-                <img
-                  className="w-6 h-6"
-                  src={require("assets/images/instagram.svg")}
-                  alt="Instagram Icon"
-                />
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </footer>
-  );
+            </div>
+        </footer>
+    );
 };
 
 Footer.defaultProps = {
-  menus: null,
+    menus: null,
 };
 
 Footer.propTypes = {
-  menus: PropTypes.any,
+    menus: PropTypes.any,
 };
 
 export default Footer;
